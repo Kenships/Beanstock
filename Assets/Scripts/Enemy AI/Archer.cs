b3b8c76faf7547f6d046f70e70c840a2b70e8a6e
RE-Mednikov <medni.arie@gmail.com>
2025-01-26 22:07:02 -0500

    Archer Tracking Code Complete

--- Assets/Scripts/Enemy AI/Archer.cs
+++ Assets/Scripts/Enemy AI/Archer.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class Archer : MonoBehaviour
 {
     public Transform player;
+    public float visionDistance;
+    public Transform forwardTip;
+
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,14 +17,22 @@ public class Archer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = GetDirectionToPlayer();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
+        if (Vector3.Distance(player.position, transform.position) < visionDistance)
+        {
+            LookAtPlayer();
+        }
     }
 
     Vector3 GetDirectionToPlayer()
     {
         return (player.position - transform.position).normalized;
+    }
+
+    private void LookAtPlayer()
+    {
+        Vector3 direction = GetDirectionToPlayer();
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 5f);
     }
 }
